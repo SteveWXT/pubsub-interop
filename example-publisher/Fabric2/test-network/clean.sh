67c9aab530ccbb6739f4870e0455968f38340bf4
wxt <389110211@qq.com>
2021-12-20 19:56:40 +0800

    publisher - solve docker env bug

---
+++ example-publisher/Fabric2/test-network/clean.sh
@@ -0,0 +1,10 @@
+sudo rm -rf organizations/peerOrganizations
+sudo rm -rf organizations/ordererOrganizations
+sudo rm -rf organizations/fabric-ca/ordererOrg
+sudo rm -rf organizations/fabric-ca/org1
+sudo rm -rf organizations/fabric-ca/org2
+sudo rm -rf system-genesis-block
+sudo rm -rf channel-artifacts
+
+
+
